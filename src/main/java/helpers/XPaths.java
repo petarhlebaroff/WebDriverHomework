9fa20ce23f83ba3344d4191f1a260a711c504677
petarhlebaroff <petarhlebaroff@gmail.com>
2023-08-24 17:37:39 +0300

    Add improvement

---
+++ src/main/java/helpers/XPaths.java
@@ -0,0 +1,13 @@
+package helpers;
+
+public class XPaths {
+
+    public static String GOOGLE_AGREE_BTN = "//button[@id='L2AGLb']";
+    public static String GOOGLE_SEARCH_FIELD = "//textarea[@type='search']";
+    public static String GOOGLE_SEARCH_BTN = "(//input[@type='submit' and @name='btnK'])[2]";
+    public static String GOOGLE_SIDE_CLICK_BTN = "//img[@alt='Google' and @class='lnXdpd']";
+    public static String GOOGLE_FIRST_RESULT = "(//a/h3)[1]";
+    public static String BING_SEARCH_BAR = "//input[@name='q' and @type='search']";
+    public static String BING_SEARCH_BTN = "//label[@id='search_icon']";
+    public static String BING_FIRST_RESULT = "(//h2)[1]";
+}
